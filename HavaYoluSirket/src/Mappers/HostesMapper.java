01834d54bbaa5a0ee0d7a4c639ca02fa93a47a6c
Serhat Rıfat Demircan <demircan.serhat@gmail.com>
2012-05-16 22:39:54 +0300

    Mappers Eklendi.

---
+++ HavaYoluSirket/src/Mappers/HostesMapper.java
@@ -0,0 +1,26 @@
+package Mappers;
+
+import java.sql.ResultSet;
+
+import TechnicalServices.AbstractDBMapper;
+
+public class HostesMapper extends AbstractDBMapper {
+
+	public HostesMapper(String tableName) {
+		super(tableName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Object getObjectFromResult(int ID, ResultSet rs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String buildObjectUpdateQuery(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
